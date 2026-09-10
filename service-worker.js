2d1bd0b8fdc7356da998d357b8a5b09899b0b4f8
trpvsk12 <trpvsk12@gmail.com>
2026-09-10 07:27:19 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="first-year-class-info-v1";
+const CACHE_NAME="first-year-class-info-v2";
 const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
 
 self.addEventListener("install",event=>{
